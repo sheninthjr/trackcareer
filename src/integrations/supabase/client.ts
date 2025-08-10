9c80c9131e4e242460aff7b7c6aaf581d939110b
sheninthjr <sheninthjr23@gmail.com>
2025-08-10 20:18:36 +0530

    Client.ts file publishablekey added

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
